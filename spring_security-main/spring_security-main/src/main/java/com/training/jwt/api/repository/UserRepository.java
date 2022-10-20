0276e1d6e4caa757a866ddbace7abe3a9c26420a
yogibala <mby02082001@gmail.com>
2022-10-20 15:35:17 +0530

    Initial Commit

---
+++ spring_security-main/spring_security-main/src/main/java/com/training/jwt/api/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.training.jwt.api.repository;
+
+import com.training.jwt.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUserName(String username);
+}
---
+++ src/main/java/com/example/SpringSecurity/demo/SpringBasicSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringSecurity.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicSecurityApplication.class, args);
+	}
+}
